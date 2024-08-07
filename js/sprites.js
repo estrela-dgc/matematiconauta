f3704e3e8da02156a1ea4fa8e69e4eddb257ad2d
estrela-dgc <danieleguitrino@gmail.com>
2024-08-07 15:01:17 +0000

    construindo f 1

---
+++ js/sprites.js
@@ -0,0 +1,14 @@
+// Forma de objetos 
+ class Sprites {
+    //Metado Construtor 
+   constructor(posX, posY, widtth, height, color) {}
+    //Posição inicial o eixo X
+    this.posX = posX; 
+    //posição inicial o eixo Y
+    this.posY = posY
+    //
+
+
+
+
+ }
